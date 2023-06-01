10b56ead63784f9c2acf9b6646a472b700ee0327
AlphaSoundZ <jan.jacob.holst@gmail.com>
2023-06-02 00:10:43 +0200

    ✨ feat: prototype

--- main.js
+++ main.js
@@ -1,171 +1,179 @@
 let result = document.getElementById("result");
-let defaultArray;
-let answer = [];
 
-let amountOfGroups = amountOfStudents / groupSize;
+let options = [{"name": "A", "amount": 0}, {"name": "B", "amount": 0}, {"name": "C", "amount": 0}];
+let maxOptionSize = 2;
 
-function rounds(array, amountOfStudents, groupSize) {
-    if (amountOfStudents == groupSize) {
-        return array;
-    }
-    
-    let result = [];
-    // first round
-    result.push(fullCopy(defaultArray));
+let alreadyChecked = [];
 
-    amountOfGroups = amountOfStudents / groupSize;
-    
-    for (let i = 0; i < amountOfGroups-1; i++) { // iterate through rounds
-        for (let j = 1; j < groupSize; j++) { // iterate through columns
-            shiftDown(array, j, j);
-        }
-        result.push(fullCopy(array));
-    }
-    
-    return result;
-}
+let electionResults = [
+    { name: "Bob", choice: ["A", "B"] },
+    { name: "Jake", choice: ["A", "B"] },
+    { name: "Lisa", choice: ["A", "C"] },
+    { name: "John", choice: ["C", "A"] },
+    { name: "Mary", choice: ["B", "C"] },
+    { name: "Mike", choice: ["A", "C"] },
+]
 
-function shiftDown(array, column, n) {
-    let temp = getColumn(array, column);
-    for (let i = 0; i < n; i++) {
-        let last = temp[temp.length - 1]; // get last element
-        temp.pop(); // remove last element
-        temp.unshift(last); // add last element to the beginning
-    }
-    // put temp back into array
-    for (let i = 0; i < temp.length; i++) {
-        array[i][column] = temp[i];
-    }
-    return array;
-}
+// every index from electionResults
+let notYetAssigned = electionResults.map((x, i) => i);
 
-function createArray(amountOfStudents, groupSize) {
-    let result = [];
-    let student = 1;
-    
-    for (let i = 0; student <= amountOfStudents; i++) {
-        result[i] = [];
-        for (let j = 0; j < groupSize; j++) {
-            result[i][j] = student;
-            student++;
-        }
-    }
+// {person: 0, option: "A"}
+let assigned = [
+];
 
-    // shuffle
-    shuffleArray(result);
-    
-    return result;
-}
+let unluckyPersons = [];
 
-function getColumn(array, column) {
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-        result[i] = array[i][column];
-    }
-    return result;
-}
+validate()
 
-function fullCopy(currentArray) {
-    var newArray = [];
-    for (var i = 0; i < currentArray.length; i++)
-        newArray[i] = currentArray[i].slice();
-    return newArray;
-}
+function validate() {
+    //e.preventDefault();
 
-function validate(e) {
-    e.preventDefault();
+    while (notYetAssigned.length > 0)
+    {
 
-    amountOfStudents = document.getElementById("amountOfStudents").value;
-    groupSize = document.getElementById("groupSize").value;
-    let radioButton = document.getElementById("amount").checked;
+        let randomIndex = Math.floor(Math.random() * notYetAssigned.length);
+        // get real index from electionResults
+        let randomPersonIndex = notYetAssigned[randomIndex];
+        let randomPerson = electionResults[randomPersonIndex];
 
-    // round up to the nearest multiple of groupSize
-    let wishedAmountsOfStudents = amountOfStudents;
-    amountOfStudents = Math.ceil(amountOfStudents / groupSize) * groupSize;
+        
+        let PersonChoice = randomPerson.choice;
 
-    // convert amount of groups to groupSize
-    if (radioButton)
-        groupSize = amountOfStudents / groupSize;
-    
-    defaultArray = createArray(amountOfStudents, groupSize);
+        // Check from first to last choice
+        let option = checkChoices(PersonChoice);
+        if (option !== false) { // result could be index 0 instead of false
+            moveToAssigned(randomPersonIndex, PersonChoice[option]);
+            console.log(notYetAssigned)
+        } else // check for exchange loop
+        {
+            let result = false;
+            for (let i = 0; i < PersonChoice.length; i++) {
+                // clear alreadyChecked
+                alreadyChecked = [];
 
-    answer = rounds(fullCopy(defaultArray), amountOfStudents, groupSize);
-
-    console.log(answer);
-    
-    result = document.getElementById("result");
-    // delete all children of result
-    while (result.firstChild) {
-        result.removeChild(result.firstChild);
-    }
-
-    // create new children
-    let rounds_title = document.createElement("h2");
-    rounds_title.innerHTML = "Rounds";
-    result.appendChild(rounds_title);
-
-
-    for (let i = 0; i < answer.length; i++) {
-        let title = document.createElement("h3");
-        title.innerHTML = "Round " + (i+1);
-        result.appendChild(title);
-
-        let table = document.createElement("table");
-        table.className = "table table-bordered";
-        result.appendChild(table);
-
-        let thead = document.createElement("thead");
-        table.appendChild(thead);
-
-        let tr = document.createElement("tr");
-        thead.appendChild(tr);
-
-        for (let j = 0; j < amountOfGroups; j++) {
-            let th = document.createElement("th");
-            th.innerHTML = "Group " + (j+1);
-            tr.appendChild(th);
-        }
-
-        let tbody = document.createElement("tbody");
-        table.appendChild(tbody);
-
-        for (let j = 0; j < groupSize; j++) { // row
-            let tr = document.createElement("tr");
-            tbody.appendChild(tr);
-            for (let k = 0; k < amountOfGroups; k++) { // column
-                let td = document.createElement("td");
-                if (answer[i][k][j] > wishedAmountsOfStudents)
+                if (findExchangeLoop(randomPersonIndex, PersonChoice[i], PersonChoice[i]))
                 {
-                    continue;
-                }    
-                    
-                td.innerHTML = answer[i][k][j];
-                
-                tr.appendChild(td);
+                    console.log("Exchange loop found")
+                    result = true;
+                    break;
+                }
+            }
+
+            if (!result)
+            {
+                // delete random person from notYetAssigned and move to seperate array
+                unluckyPersons.push(randomIndex);
+                notYetAssigned.splice(randomIndex, 1);
+            } 
+        }
+    }
+
+    // show results
+    result.innerHTML = "";
+    for (let i = 0; i < options.length; i++) {
+        result.innerHTML += options[i].name + ": " + options[i].amount + "<br/>";
+
+        // show persons
+        let persons = assigned.filter(x => x.option == options[i].name);
+        for (let j = 0; j < persons.length; j++) {
+            result.innerHTML += electionResults[persons[j].personIndex].name + "<br/>";
+        }
+        result.innerHTML += "<br/>";
+
+    }
+
+    console.log("Unlucky persons: " + unluckyPersons.length);
+}
+
+function findExchangeLoop(exchanger, exchangerChoice, originalChoice) {
+
+    // check if exchanger has already been checked
+    if (alreadyChecked.includes(exchanger))
+    {
+        return false;
+    } else
+    {
+        alreadyChecked.push(exchanger);
+    }
+
+    // check if exchanger's choice is free
+    if (lookupOption(exchangerChoice) < maxOptionSize)
+    {
+        console.log("found!")
+        changeAssignment(exchanger, exchangerChoice);
+        return true;
+    }
+    
+    // iterate through persons who are assigned to the exchanger's choice
+    let persons = assigned.filter(x => x.option == originalChoice);
+
+    for (let i = 0; i < persons.length; i++) {
+        let person = persons[i];
+
+        // person's other choices (without the exchanger's choice)
+        let personChoices = electionResults[person.personIndex].choice.filter(x => x != exchangerChoice && x != originalChoice);
+
+        for (let j = 0; j < personChoices.length; j++) { // iterate through person's choices (without the exchanger's choice)
+            // recursive call to check if the person can exchange
+            let option = checkChoices(personChoices);
+            if (option !== false) {
+                changeAssignment(person.personIndex, personChoices[option]);
+                return true;
+            } else
+            {
+                for (let k = 0; k < personChoices.length; k++) {
+                    if (findExchangeLoop(person.personIndex, personChoices[k], originalChoice))
+                    {
+                        changeAssignment(person.personIndex, personChoices[k]);
+                        return true;
+                    }
+                }
             }
         }
 
-        // table style with borders
-        let style = document.createElement("style");
-        style.innerHTML = "table, th, td { border: 1px solid black; padding: 5px;}";
-        result.appendChild(style);
+        return false;
     }
+
+        
 }
 
-function shuffleArray(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
+function moveToAssigned(index, option) {
+    assigned.push({"personIndex": index, "option": option});
+    
+    // delete from notYetAssigned where element is index
+    notYetAssigned.splice(notYetAssigned.indexOf(index), 1);
+
+    options.find(x => x.name == option).amount++;
+}
+
+function changeAssignment(index, newOption) {
+    let person = assigned.find(x => x.personIndex == index)
+
+    // Remove from old option counter
+    options.find(x => x.name == person.option).amount--;
+
+    // Add to new option counter
+    options.find(x => x.name == newOption).amount++;
+
+    // Change assignment
+    person.option = newOption;
+}
+
+// Returns the amount of votes for a given option
+function lookupOption(optionName) {
+    let option = options.find(x => x.name == optionName);
+    return option.amount;
+}
+
+function checkChoices (PersonChoice) {
+    let amountOfChoices = PersonChoice.length;
+
+    for (let i = 0; i < amountOfChoices; i++) {
+        if (lookupOption(PersonChoice[i]) < maxOptionSize) // If first choice is available
+        {
+            return i;
+        }
+    };
+
+    return false;
+}
